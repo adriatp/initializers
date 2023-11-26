9b07092eb96511ad3bd95e2f9b90d603af73e75d
adriatp <tarradas.adria@gmail.com>
2023-11-26 12:36:35 +0100

    Add disk model into dialog

--- init_arc.sh
+++ init_arc.sh
@@ -25,6 +25,6 @@ password2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || e
 clear
 [[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
 
-devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
-device=$(dialog --stdout --menu "Select installtion disk" 0 0 0 ${devicelist}) || exit 1
+devicelist=$(lsblk -dplnx size -o name,model,size | grep -Ev "boot|rpmb|loop" | awk -F '\t' '{ $2=gensub(/ /, "_", "g", $2); print $1 " " $2 "(" $3 ")"}' | tac)
+device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
 clear
