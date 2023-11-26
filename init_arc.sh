11fbdd9966a216776118350c5320892b68520f72
adriatp <tarradas.adria@gmail.com>
2023-11-26 13:08:33 +0100

    Set up partitions

--- init_arc.sh
+++ init_arc.sh
@@ -2,9 +2,16 @@
 # How to run: curl -sL <link raw github> | bash 
 # Inspired by: https://disconnected.systems/blog/archlinux-installer/#setting-variables-and-collecting-user-input
 
+# Setup keyboard layout and timezone
+loadkeys es
+timedatectl set-timezone Europe/Andorra
+timedatectl set-ntp true
+
+# Redirect fails and display error msg 
 set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
+# Init pacman keys and download dialog
 pacman-key --init
 pacman -Sy dialog --noconfirm
 
@@ -28,3 +35,13 @@ clear
 devicelist=$(lsblk -dplnx size -o name,model,size | grep -Ev "boot|rpmb|loop" | awk -F '\t' '{ $2=gensub(/ /, "_", "g", $2); print $1 " " $2 "(" $3 ")"}' | tac)
 device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
 clear
+
+# Set up logging
+exec 1> >(tee "stdout.log")
+exec 2> >(tee "stderr.log")
+
+# Set up partitions
+parted --script "${device}" -- mklabel gpt \
+  mkpart ESP fat32 1MiB 129MiB \
+  set 1 boot on \
+  mkpart primary ext4 129MiB 100%
