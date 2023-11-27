3f370b3caa1038f4c2314d3e837f87b6a33abc22
adriatp <tarradas.adria@gmail.com>
2023-11-27 02:33:01 +0100

    Remove one yay installation

--- arc/nvidia.sh
+++ arc/nvidia.sh
@@ -7,8 +7,7 @@ set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 # Install driver packages
-yay -S nvidia nvidia-utils lib32-nvidia
-yay -S nvidia-settings
+yay -S nvidia nvidia-utils lib32-nvidia nvidia-settings
 
 # Enable DRM kernel mode setting
 sudo sed -i '/options/ s/$/ nvidia-drm.modeset=1/' /boot/loader/entries/arch.conf
