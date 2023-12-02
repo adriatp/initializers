7a43e775c91445a56a473c34fc71fafe2d6cb556
adriatp <tarradas.adria@gmail.com>
2023-12-02 17:26:38 +0100

    Enable pw services

--- arc/install.sh
+++ arc/install.sh
@@ -85,6 +85,7 @@ EOF
 
 # Startup with net and audio
 arch-chroot /mnt systemctl enable NetworkManager
+arch-chroot /mnt systemctl --user enable pipewire.service pipewire-pulse.service
 arch-chroot /mnt systemctl --user enable pipewire pipewire-pulse wireplumber
 
 # Update fstab with secured mask
