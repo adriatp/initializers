2dcacb0fd2997cc60dd132052de511e8057b983c
adriatp <tarradas.adria@gmail.com>
2023-12-03 15:35:09 +0100

    Update kitty-themes

---
+++ arc/kitty.sh
@@ -0,0 +1,4 @@
+
+mkdir -p /home/${USER}/.config/kitty/kitty-themes
+cp kitty/kitty-themes/* /home/${USER}/.config/kitty/kitty-themes
+ln -s /home/${USER}/.config/kitty/kitty-themes/Cobalt_Neon.conf /home/${USER}/.config/kitty/theme.conf
