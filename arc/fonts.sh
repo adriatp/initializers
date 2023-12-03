41922500255e4b520e7809e8bc717ba6d52c7cec
adriatp <tarradas.adria@gmail.com>
2023-12-03 14:35:23 +0100

    Create fonts directory before unzip

--- arc/fonts.sh
+++ arc/fonts.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.local/share/fonts
 cp fonts/* ~/.local/share/fonts/
 for font_zip in ~/.local/share/fonts/*.zip; do
   dir_name=$(basename "$font_zip" .zip)
+  mkdir -p "~/.local/share/fonts/${dir_name}"
   unzip "${font_zip}" -d "~/.local/share/fonts/${dir_name}"
   rm "~/.local/share/fonts/$font_zip"
 done
