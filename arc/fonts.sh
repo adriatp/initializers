d701ba089658f5841d9d4546c8d81ab702eda682
adriatp <tarradas.adria@gmail.com>
2023-12-03 14:33:09 +0100

    Fix relative paths

--- arc/fonts.sh
+++ arc/fonts.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.local/share/fonts
 cp fonts/* ~/.local/share/fonts/
 for font_zip in ~/.local/share/fonts/*.zip; do
   dir_name=$(basename "$font_zip" .zip)
-  unzip "$font_zip" -d "$dir_name"
-  rm "$font_zip"
+  unzip "${font_zip}" -d "~/.local/share/fonts/${dir_name}"
+  rm "~/.local/share/fonts/$font_zip"
 done
 fc-cache -f -v
