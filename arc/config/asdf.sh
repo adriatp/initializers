24cc6d8af82fb5dc7cff1e802099043911afdbba
adriatp <tarradas.adria@gmail.com>
2023-12-05 11:29:57 +0100

    Avoid install versions with asdf script

--- arc/config/asdf.sh
+++ arc/config/asdf.sh
@@ -7,42 +7,41 @@ cat << EOF >> ~/.bashrc
 # Asdf
 source /opt/asdf-vm/asdf.sh
 EOF
-source ~/.bashrc
 
-# Install ruby
+# # Install ruby
 
-## List ruby related plugins
-asdf plugin list all | grep ruby
+# ## List ruby related plugins
+# asdf plugin list all | grep ruby
 
-## Install ruby
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+# ## Install ruby
+# asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 
-## List all ruby versions
-asdf list all ruby
+# ## List all ruby versions
+# asdf list all ruby
 
-## Install latest ruby version
-asdf install ruby latest
-adsf global ruby latest
+# ## Install latest ruby version
+# asdf install ruby latest
+# adsf global ruby latest
 
-# Install gems
+# # Install gems
 
-## Install bundler
-echo "gem: --no-document" > ~/.gemrc
-gem search '^bundler$' --all
-gem install bundler latest
+# ## Install bundler
+# echo "gem: --no-document" > ~/.gemrc
+# gem search '^bundler$' --all
+# gem install bundler latest
 
-## Install rails
-gem search '^rails$' --all
-gem install rails -v latest
+# ## Install rails
+# gem search '^rails$' --all
+# gem install rails -v latest
 
-## Install node (with npm)
-asdf plugin list all | grep node
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install nodejs latest
-asdf global nodejs latest
+# ## Install node (with npm)
+# asdf plugin list all | grep node
+# asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+# asdf install nodejs latest
+# asdf global nodejs latest
 
-## Install yarn
-asdf plugin list all | grep yarn
-asdf plugin add yarn https://github.com/twuni/asdf-yarn.git
-asdf install yarn 1.22.19
-asdf global yarn 1.22.19
+# ## Install yarn
+# asdf plugin list all | grep yarn
+# asdf plugin add yarn https://github.com/twuni/asdf-yarn.git
+# asdf install yarn 1.22.19
+# asdf global yarn 1.22.19
