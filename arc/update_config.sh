e2775e8e21b238c9140d62f7a8cbb9437e77d733
adriatp <tarradas.adria@gmail.com>
2023-12-07 14:01:48 +0100

    Fix update_config script

--- arc/update_config.sh
+++ arc/update_config.sh
@@ -3,10 +3,11 @@
 repo_dir="arc/config/dotfiles/"
 os_dir="/home/${USER}/"
 find "$repo_dir" -type f | while read -r file; do
-  relative_path="${file#$repo_dir/}"
+  relative_path="${file#$repo_dir}"
   os_path="${os_dir}${relative_path}"
   repo_path="${repo_dir}${relative_path}"
-  if [ -e "$os_path"]; then
+  if [ -e "$os_path" ]; then
+    echo "Copying $os_path to $repo_path"
     cp $os_path $repo_path
   fi
 done
