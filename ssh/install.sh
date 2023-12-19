881fcc8b211b0f6cb4b2147df65772c6b2fd8ac3
adriatp <tarradas.adria@gmail.com>
2023-12-19 20:14:25 +0100

    Restart sshd after installation

--- ssh/install.sh
+++ ssh/install.sh
@@ -11,3 +11,6 @@ echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config > /dev/null
 # Copy authorized_keys
 mkdir -p ~/.ssh
 curl -o ~/.ssh/authorized_keys https://raw.githubusercontent.com/adriatp/initializers/main/ssh/authorized_keys
+
+# Restart sshd
+sudo systemctl restart sshd
